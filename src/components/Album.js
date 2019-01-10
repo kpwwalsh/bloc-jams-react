a7d2a6a246feb05fbb833ded5864033f2d0f110c
kpwwalsh <wnuru@hotmail.com>
2019-01-10 14:44:26 -0600

    Checkpoint jams audio: completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -11,8 +11,38 @@ class Album extends Component {
       });
   
       this.state = {
-        album: album
+        album: album,
+        currentSong: album.songs[0],
+        isPlaying: false
       };
+
+      this.audioElement = document.createElement('audio');
+      this.audioElement.src = album.songs[0].audioSrc;
+      }
+
+      play() {
+      this.audioElement.play();
+      this.setState({ isPlaying: true });
+      }
+
+      pause() {
+      this.audioElement.pause();
+      this.setState({ isPlaying: false });
+     }   
+
+      setSong(song) {
+      this.audioElement.src = song.audioSrc;
+      this.setState({ currentSong: song });
+    } 
+
+      handleSongClick(song) {
+      const isSameSong = this.state.currentSong === song;
+      if (this.state.isPlaying && isSameSong) {
+        this.pause();
+      } else {
+        if (!isSameSong) { this.setSong(song); }  
+        this.play();
+      }
     }
 
     render() {
@@ -36,7 +66,7 @@ class Album extends Component {
            {
             this.state.album.songs.map( (song, index) => {
               return (
-               <tr key={index}>
+                <tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
                   <td className= "songs-number">{index+1}</td>
                   <td className= "songs-title">{song.title}</td>
                   <td className= "songs-duration">{song.duration}</td> 
