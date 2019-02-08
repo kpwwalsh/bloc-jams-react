7127178df7e331c0926646ac88570a5582b11fd8
kpwwalsh <wnuru@hotmail.com>
2019-02-08 17:17:25 -0600

    Assignment jams inputs completed

--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -17,22 +17,28 @@ class PlayerBar extends Component {
            </button>
          </section>
          <section id="time-control">
-         <div className="current-time">{this.props.currentTime}</div>
+         <div className="current-time">{this.props.currentTimeLabel}</div>
            <input 
              type="range" 
              className="seek-bar" 
-             value={(this.props.currentTime / this.props.duration) || 0} 
+             value={(this.props.currentTime / this.props.duration|| 0) } 
              max="1" 
              min="0" 
              step="0.01" 
              onChange={this.props.handleTimeChange}
            />   
-           <div className="total-time">{this.props.duration}</div> 
+           <div className="total-time">{this.props.totalTime}</div> 
          </section>
          <section id="volume-control">
-           <div className="icon ion-volume-low"></div>
-           <input type="range" className="seek-bar" value="80" />
-           <div className="icon ion-volume-high"></div>
+           <div className="current-volume">{this.props.volume}</div>
+           <input type="range" 
+           className="seek-bar" 
+           value= {this.props.volume} 
+           max= "1"
+           min= "0"
+           step=".01"
+           onChange={this.props.handleVolumeChange}
+           />
          </section>
       </section>
     );
